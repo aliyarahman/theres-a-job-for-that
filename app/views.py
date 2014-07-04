ffc5757420612e2ea2e5055221e583d3a80125d7
aliyarahman <aliya.rahman@yahoo.com>
2014-07-04 02:30:39 -0400

    Added question views

--- app/views.py
+++ app/views.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+import random
 from app import app	# Go get tools we need from the app folder
 from flask import render_template 	# So that we can render html files in our app
 
@@ -7,3 +8,28 @@ from flask import render_template 	# So that we can render html files in our app
 @app.route('/index')	# This means this is the view for the landing page
 def index():
 	return render_template('index.html')
+
+@app.route('/i_can')
+def i_can():
+	can_questions = ['I can drive a car', 'I can speak Spanish', 'I can fix a carburetor', 'I can plant a tree']
+	can_question = random.choice(can_questions)
+	return render_template('can_question.html', question=can_question)
+
+@app.route('/i_want')
+def i_want():
+        want_questions = ['I want to work outdoors', 'I want to work with seniors', 'I want to learn new skills']
+        want_question = random.choice(want_questions)
+        return render_template('want_question.html', question=want_question)
+
+@app.route('/i_need')
+def i_need():
+        need_questions = ['I need flexible work hours', 'I need trans inclusive health care', 'I need a veteran friendly employer', 'I need an employer who might hire people with criminal records']
+        need_question = random.choice(need_questions)
+        return render_template('need_question.html', question=need_question)
+
+
+
+@app.route('/jobView/<id>')
+def jobView(id):
+	job = models.Job.query.get(id)
+	return render_template('jobView.html', job=job)
