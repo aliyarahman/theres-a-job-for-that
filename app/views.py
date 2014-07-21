cd881fc5b8c6739763173bcdfe58eb643b3ee27b
aliyarahman <aliya.rahman@yahoo.com>
2014-07-21 14:36:10 -0400

    Added login page

--- app/views.py
+++ app/views.py
@@ -5,6 +5,10 @@ from app import app	# Go get tools we need from the app folder
 from flask import render_template 	# So that we can render html files in our app
 
 @app.route('/')
+@app.route('/login')
+def login():
+	return render_template('login.html')
+
 @app.route('/index')	# This means this is the view for the landing page
 def index():
 	return render_template('index.html')
