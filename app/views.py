001b86907478378cc296ebf8b52edcc9fd7389b2
aliyarahman <aliya.rahman@yahoo.com>
2014-07-23 13:15:57 -0400

    Added logic to count through questions

--- app/views.py
+++ app/views.py
@@ -2,7 +2,7 @@
 
 import random
 from app import app	# Go get tools we need from the app folder
-from flask import render_template 	# So that we can render html files in our app
+from flask import render_template, redirect 	# So that we can render html files in our app
 
 @app.route('/')
 @app.route('/login')
@@ -17,23 +17,35 @@ def index():
 # and query be category on the homepage button click (e.g. they click "I Can" and it passes "can" as category
 # into db query
 
-@app.route('/i_can')
-def i_can():
+@app.route('/i_can/<can_counter>')
+def i_can(can_counter):
+	counter=int(can_counter)
+	if counter==3:
+		return redirect('index')
+	counter+=1
 	can_questions = ['I can drive a car', 'I can speak Spanish', 'I can fix a carburetor', 'I can plant a tree']
-	can_question = random.choice(can_questions)
-	return render_template('can_question.html', question=can_question)
+	can_question = can_questions[counter]
+	return render_template('can_question.html', question=can_question, can_counter=counter)
 
-@app.route('/i_want')
-def i_want():
-        want_questions = ['I want to work outdoors', 'I want to work with seniors', 'I want to learn new skills']
-        want_question = random.choice(want_questions)
-        return render_template('want_question.html', question=want_question)
+@app.route('/i_want/<want_counter>')
+def i_want(want_counter):
+	counter=int(want_counter)
+	if counter==3:
+		return redirect('index')
+	counter+=1
+	want_questions = ['I want to work outdoors', 'I want to work on a team','I want to work with seniors', 'I want to learn new skills']
+	want_question = want_questions[counter]
+	return render_template('want_question.html', question=want_question, want_counter=counter)
 
-@app.route('/i_need')
-def i_need():
-        need_questions = ['I need flexible work hours', 'I need trans inclusive health care', 'I need a veteran friendly employer', 'I need an employer who might hire people with criminal records']
-        need_question = random.choice(need_questions)
-        return render_template('need_question.html', question=need_question)
+@app.route('/i_need/<need_counter>')
+def i_need(need_counter):
+	counter=int(need_counter)
+	if counter==3:
+		return redirect('matching')
+	counter+=1
+	need_questions = ['I need flexible work hours', 'I need trans inclusive health care', 'I need a veteran friendly employer', 'I need an employer who might hire people with criminal records']
+	need_question = need_questions[counter]
+	return render_template('need_question.html', question=need_question, need_counter= counter)
 
 
 # Notification of skill language generated
