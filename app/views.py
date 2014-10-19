3ec9c7a0bbf5bd404faaf4d9289bd1edc5720607
aliyarahman <aliya.rahman@yahoo.com>
2014-10-19 19:57:28 -0400

    Integrated three new job types, added button remove jquery methods

--- app/views.py
+++ app/views.py
@@ -34,59 +34,60 @@ def location():
     form = LocationForm()
     if form.validate_on_submit():
         session['location'] = form.city.data+", "+form.state.data
-        return redirect('/start')
+        session['job_categories'] = []
+        session['jobs'] =[]
+        session['context']=[]
+        session['all_jobs'] = []
+        session['search_strings'] = ['retail sales', 'warehouse worker', 'caregiver', 'data entry']
+        session['this_job'] = 'retail sales'
+        return render_template('start.html', category=session['this_job'])
     return render_template('location.html', form=form)
 
 
-@app.route('/start')
-def start():
-    session['search_string'] ="retail sales"
-    session['context']=[]
-    return render_template('start.html')
+@app.route('/start/<category>')
+def start(category):
+    if category != 'pass':
+        session['job_categories'].append(category)
+    session['search_strings'].remove(session['this_job'])
+    if len(session['search_strings'])>0:
+        session['this_job'] = session['search_strings'][0]
+        return render_template('start.html', category = session['this_job'])
+    else:
+        return redirect(url_for('question_view',swipe=2))
 
 
 @app.route('/question_view/<swipe>')
 def question_view(swipe):
     if swipe=='2':
-        q = { "keyword": session['search_string'], "location": session['location'], "context":session['context']}
+        q = { "keyword": session['job_categories'][0], "location": session['location'], "context":session['context']}
         r = api_call(q)
         session['question']=r['question']
         return render_template('question.html', question=session['question'])
     else:
         session['context'].append({"question":session['question'], "answer":int(swipe)})
-        q = { "keyword": session['search_string'], "location": session['location'], "context":session['context']}
+        q = { "keyword": session['job_categories'][0], "location": session['location'], "context":session['context']}
         r = api_call(q)
         if 'jobs' not in r.keys():
             session['question']=r['question']
             return render_template('question.html', question = session['question'])
         else:
-        	session['jobs'] = r['jobs']
-        	session['job_count'] = r['n_jobs']
-        	return redirect(url_for('myjobs'))
-
-
-@app.route('/matching')
-def skills():
-	return render_template('matching.html')
+            obj = xmltodict.parse(r['jobs'])
+            jobs_obj = obj['rs']['r']
+            for j in jobs_obj:
+                description = j['e'][0:-1]
+                for i in [['01','January'],['02','February'],['03','March'],['04','April'],['05','May'],['06','June'],['07','July'],['08','August'],['09','September'],['10','October'],['11','November'],['12','December']]:
+                    if j['dp'][5:7] == i[0]:
+                        month = i[1]
+                date_string = month+" "+j['dp'][8:10]+", "+j['dp'][0:4]
+                session['all_jobs'].append({'title':j['jt'],'company_url':j['cn'],'posting_url':j['src']['@url'],'posting_source':j['src']['#text'],'location':j['loc']['#text'],'date_posted': date_string,'description':description})
+            session['job_categories'].remove(session['job_categories'][0])
+            if len(session['job_categories'])==0:
+                return redirect(url_for('myjobs'))
+            else:
+                return redirect(url_for('question_view',swipe=2))
 
 
 @app.route('/myjobs')
-def myjobs():
-    obj = xmltodict.parse(session['jobs'])
-    jobs_obj = obj['rs']['r']
-    jobs=[]
-    for j in jobs_obj:
-        description = j['e'][0:-1]
-        for i in [['01','January'],['02','February'],['03','March'],['04','April'],['05','May'],['06','June'],['07','July'],['08','August'],['09','September'],['10','October'],['11','November'],['12','December']]:
-            if j['dp'][5:7] == i[0]:
-                month = i[1]
-        date_string = month+" "+j['dp'][8:10]+", "+j['dp'][0:4]
-        jobs.append({'title':j['jt'],
-            'company_url':j['cn'],
-            'posting_url':j['src']['@url'],
-            'posting_source':j['src']['#text'],
-            'location':j['loc']['#text'],
-            'date_posted': date_string,
-            'description':description})
-    job_count = len(jobs)
-    return render_template('myjobs.html', jobs=jobs, job_count=job_count)
+def myjobs(): 
+    job_count = len(session['all_jobs'])
+    return render_template('myjobs.html', jobs=session['all_jobs'], job_count=job_count)
